2dd38b1b3893647728608918c1d5185bcb1d1221
chino abatol <chinoabatol@chinos-Mac.local>
2019-03-02 15:47:36 -0800

    adding files

---
+++ InstaParse/PostCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCell.swift
+//  InstaParse
+//
+//  Created by chino abatol on 2/28/19.
+//  Copyright © 2019 chino abatol. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
